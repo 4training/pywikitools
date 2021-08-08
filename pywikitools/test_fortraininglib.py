e0aeaaadd2ca7177e382f0807448314bfa94914c
holybiber <holybiber@4training.net>
2021-08-08 12:13:33 +0200

    Testing improved: logging
    Documentation on how to enable logging while developing tests.
    Introduced assertLogs() to check that a function is logging something as
    expected.

--- pywikitools/test_fortraininglib.py
+++ pywikitools/test_fortraininglib.py
@@ -6,22 +6,29 @@ Run tests:
     python3 -m unittest test_fortraininglib.py
 """
 import unittest
+import logging
 import sys
 sys.path.append('../')
 # TODO import that without the dirty hack above
 import fortraininglib
 
 class TestFortrainingLib(unittest.TestCase):
+# use this to see logging messages (can be increased to logging.DEBUG)
+#    def setUp(self):
+#        logging.basicConfig(level=logging.INFO)
+
     def test_get_language_name(self):
         self.assertEqual(fortraininglib.get_language_name('de'), 'Deutsch')
         self.assertEqual(fortraininglib.get_language_name('en'), 'English')
         self.assertEqual(fortraininglib.get_language_name('tr'), 'Türkçe')
-        self.assertEqual(fortraininglib.get_language_name('de','en'), 'German')
-        self.assertEqual(fortraininglib.get_language_name('tr','de'), 'Türkisch')
+        self.assertEqual(fortraininglib.get_language_name('de', 'en'), 'German')
+        self.assertEqual(fortraininglib.get_language_name('tr', 'de'), 'Türkisch')
 
     def test_list_page_translations(self):
-        result = fortraininglib.list_page_translations('Prayer')
-        result_with_incomplete = fortraininglib.list_page_translations('Prayer', include_unfinished=True)
+        with self.assertLogs('4training.lib', level='INFO'):
+            result = fortraininglib.list_page_translations('Prayer')
+        with self.assertLogs('4training.lib', level='INFO'):
+            result_with_incomplete = fortraininglib.list_page_translations('Prayer', include_unfinished=True)
         self.assertTrue(len(result) >= 5)
         self.assertTrue(len(result) <= len(result_with_incomplete))
         for language, progress in result.items():
