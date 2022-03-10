d4e54159eaf6b977cb040b25677ff36b40972608
holybiber <samuel@holydevelopers.net>
2022-03-10 09:15:07 +0100

    Simplified code in class ConsistencyCheck
    Using the already existing TranslationUnit class makes everything much
    better.

--- pywikitools/resourcesbot/consistency_checks.py
+++ pywikitools/resourcesbot/consistency_checks.py
@@ -5,6 +5,7 @@ Contains consistency checks specifically for 4training.net
 import logging
 from typing import Final, Optional, Union
 from pywikitools import fortraininglib
+from pywikitools.lang.translated_page import TranslationUnit
 from pywikitools.resourcesbot.changes import ChangeLog
 from pywikitools.resourcesbot.data_structures import LanguageInfo, WorksheetInfo
 from pywikitools.resourcesbot.post_processing import LanguagePostProcessor
@@ -16,97 +17,78 @@ class ConsistencyCheck(LanguagePostProcessor):
     def __init__(self):
         self.logger = logging.getLogger("pywikitools.resourcesbot.consistency_checks")
 
-    def make_link(self, page: WorksheetInfo, translation_unit: Union[str, int]) -> str:
-        return f"{fortraininglib.BASEURL}/Translations:{page.page}/{translation_unit}/{page.language_code}"
+    def load_translation_unit(self, language_info: LanguageInfo, page: str,
+            identifier: Union[int, str]) -> Optional[TranslationUnit]:
+        """
+        Try to load a translation unit
 
-    def _load_content(self, page: WorksheetInfo, translation_unit: Union[str, int]) -> str:
-        if isinstance(translation_unit, int):
-            content = fortraininglib.get_translated_unit(page.page, page.language_code, translation_unit)
+        If we request the title of a worksheet, let's first try to see if it's already in language_info.
+        Then we don't need to make an API query.
+        Otherwise we try to load the translation unit from the mediawiki system
+        """
+        if isinstance(identifier, int):
+            content = fortraininglib.get_translated_unit(page, language_info.get_language_code(), identifier)
             if content is None:
-                raise LookupError(f"Couldn't get {self.make_link(page, translation_unit)}")
-            return content
-        elif translation_unit == self.TITLE:
-            return page.title
-        else:
-            raise LookupError(f"Invalid unit name {page.page}/{translation_unit}/{page.language_code}")
+                self.logger.info(f"Couldn't load {page}/{identifier}/{language_info.get_language_code()}")
+                return None
+            return TranslationUnit(f"{page}/{identifier}", language_info.get_language_code(),
+                "", content) # Leaving definition parameter empty because we don't have it and don't need it
 
-    def should_be_equal(self, base_page: Optional[WorksheetInfo], base_unit: Union[str, int],
-                         other_page: Optional[WorksheetInfo], other_unit: Union[str, int]):
-        if base_page is None or other_page is None:
-            return
-        base: str = self._load_content(base_page, base_unit)
-        other: str = self._load_content(other_page, other_unit)
-        if other == base:
-            self.logger.info(f"Consistency check passed: {base} == {other}")
-        else:
-            self.logger.warning(f"Consistency check failed: {other} is not equal to {base}."
-                                f" Check {self.make_link(base_page, base_unit)}"
-                                f" and {self.make_link(other_page, other_unit)}")
-
-    def should_start_with(self, base_page: Optional[WorksheetInfo], base_unit: Union[str, int],
-                         other_page: Optional[WorksheetInfo], other_unit: Union[str, int]):
-        if base_page is None or other_page is None:
-            return
-        base = self._load_content(base_page, base_unit)
-        other = self._load_content(other_page, other_unit)
-        if other.startswith(base):
-            self.logger.info(f"Consistency check passed: {other} starts with {base}.")
-        else:
-            self.logger.warning(f"Consistency check failed: {other} does not start with {base}."
-                                f" Check {self.make_link(base_page, base_unit)}"
-                                f" and {self.make_link(other_page, other_unit)}")
-
-    def should_start_with_2(self, base_page: str, language_code: str, base_unit: Union[str, int],
-                         other_page: Optional[WorksheetInfo], other_unit: Union[str, int]):
-        """TODO can this be done more elegant?"""
-        base_content: str = ""
-        if isinstance(base_unit, int):
-            content = fortraininglib.get_translated_unit(base_page, language_code, base_unit)
+        elif identifier == self.TITLE:
+            worksheet_info: Optional[WorksheetInfo] = language_info.get_worksheet(page)
+            if worksheet_info is not None:
+                return TranslationUnit(f"{page}/{language_info.get_language_code()}",
+                    language_info.get_language_code(), page, worksheet_info.title)
+            content = fortraininglib.get_translated_title(page, language_info.get_language_code())
             if content is None:
-                return
-            base_content = content
-        elif base_unit == self.TITLE:
-            content = fortraininglib.get_translated_title(base_page, language_code)
-            if content is None:
-                return
-            base_content = content
-        else:
-            raise LookupError(f"Invalid unit name {base_page}/{base_unit}/{language_code}")
+                self.logger.info(f"Couldn't load {page}/{identifier}/{language_info.get_language_code()}")
+                return None
+            return TranslationUnit(f"{page}/Page display title",
+                language_info.get_language_code(), page, content)
 
-        if other_page is None:
+        else:
+            raise LookupError(f"Invalid unit name {page}/{identifier}/{language_info.get_language_code()}")
+
+    def should_be_equal(self, base: Optional[TranslationUnit], other: Optional[TranslationUnit]):
+        if base is None or other is None:
             return
-        other = self._load_content(other_page, other_unit)
-        if other.startswith(base_content):
-            self.logger.info(f"Consistency check passed: {other} starts with {base_content}.")
+        if other.get_translation() == base.get_translation():
+            self.logger.info(f"Consistency check passed: {base.get_translation()} == {other.get_translation()}")
         else:
-            self.logger.warning(f"Consistency check failed: {other} does not start with {base_content}."
-                                f" Check {base_page}/{base_unit}/{language_code}"   # TODO
-                                f" and {self.make_link(other_page, other_unit)}")
+            self.logger.warning(f"Consistency check failed: {other.get_translation()} is not equal to "
+                                f"{base.get_translation()}. Check {base.get_name()} and {other.get_name()}")
 
+    def should_start_with(self, base: Optional[TranslationUnit], other: Optional[TranslationUnit]):
+        if base is None or other is None:
+            return
+        if other.get_translation().startswith(base.get_translation()):
+            self.logger.info(f"Consistency check passed: {other.get_translation()} starts with {base.get_translation()}.")
+        else:
+            self.logger.warning(f"Consistency check failed: {other.get_translation()} does not start with "
+                                f"{base.get_translation()}. Check {base.get_name()} and {other.get_name()}")
 
     def check_bible_reading_hints_titles(self, language_info: LanguageInfo):
         """Titles of the different Bible Reading Hints variants should start the same"""
         self.should_start_with(
-            language_info.get_worksheet("Bible_Reading_Hints"), self.TITLE,
-            language_info.get_worksheet("Bible_Reading_Hints_(Seven_Stories_full_of_Hope)"), self.TITLE)
+            self.load_translation_unit(language_info, "Bible_Reading_Hints", self.TITLE),
+            self.load_translation_unit(language_info, "Bible_Reading_Hints_(Seven_Stories_full_of_Hope)", self.TITLE))
         self.should_start_with(
-            language_info.get_worksheet("Bible_Reading_Hints"), self.TITLE,
-            language_info.get_worksheet("Bible_Reading_Hints_(Starting_with_the_Creation)"), self.TITLE)
+            self.load_translation_unit(language_info, "Bible_Reading_Hints", self.TITLE),
+            self.load_translation_unit(language_info, "Bible_Reading_Hints_(Starting_with_the_Creation)", self.TITLE))
 
     def check_gods_story_titles(self, language_info: LanguageInfo):
         """Titles of the two different variants of God's Story should start the same"""
-        self.should_start_with_2(
-            "God's_Story", language_info.get_language_code(), self.TITLE,
-            language_info.get_worksheet("God's_Story_(first_and_last_sacrifice)"), self.TITLE)
-        self.should_start_with_2(
-            "God's_Story", language_info.get_language_code(), self.TITLE,
-            language_info.get_worksheet("God's_Story_(five_fingers)"), self.TITLE)
+        self.should_start_with(
+            self.load_translation_unit(language_info, "God's_Story", self.TITLE),
+            self.load_translation_unit(language_info, "God's_Story_(first_and_last_sacrifice)", self.TITLE))
+        self.should_start_with(
+            self.load_translation_unit(language_info, "God's_Story", self.TITLE),
+            self.load_translation_unit(language_info, "God's_Story_(five_fingers)", self.TITLE))
 
     def check_who_do_i_need_to_forgive(self, language_info: LanguageInfo):
         """Should both be 'God, who do I need to forgive?'"""
-        self.should_be_equal(
-            language_info.get_worksheet("How_to_Continue_After_a_Prayer_Time"), 11,
-            language_info.get_worksheet("Forgiving_Step_by_Step"), 34)
+        self.should_be_equal(self.load_translation_unit(language_info, "How_to_Continue_After_a_Prayer_Time", 11),
+            self.load_translation_unit(language_info, "Forgiving_Step_by_Step", 34))
 
     def run(self, language_info: LanguageInfo, change_log: ChangeLog):
         self.check_bible_reading_hints_titles(language_info)
