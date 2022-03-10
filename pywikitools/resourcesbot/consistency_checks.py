7bded1dde939f34ca8bb4383f3aea18bb6ce4e84
holybiber <samuel@holydevelopers.net>
2022-03-10 16:03:23 +0100

    Add more consistency checks and tests for it
    New function extract_link()

--- pywikitools/resourcesbot/consistency_checks.py
+++ pywikitools/resourcesbot/consistency_checks.py
@@ -3,7 +3,8 @@ Contains consistency checks specifically for 4training.net
 """
 
 import logging
-from typing import Final, Optional, Union
+import re
+from typing import Final, Optional, Tuple, Union
 from pywikitools import fortraininglib
 from pywikitools.lang.translated_page import TranslationUnit
 from pywikitools.resourcesbot.changes import ChangeLog
@@ -17,8 +18,15 @@ class ConsistencyCheck(LanguagePostProcessor):
     def __init__(self):
         self.logger = logging.getLogger("pywikitools.resourcesbot.consistency_checks")
 
+    def extract_link(self, text: str) -> Tuple[str, str]:
+        match = re.search(r"\[\[([^|]+)\|([^\]]+)\]\]", text)
+        if not match:
+            return "", ""
+        return match.group(1), match.group(2)
+
+
     def load_translation_unit(self, language_info: LanguageInfo, page: str,
-            identifier: Union[int, str]) -> Optional[TranslationUnit]:
+                              identifier: Union[int, str]) -> Optional[TranslationUnit]:
         """
         Try to load a translation unit
 
@@ -31,90 +39,124 @@ class ConsistencyCheck(LanguagePostProcessor):
             if content is None:
                 self.logger.info(f"Couldn't load {page}/{identifier}/{language_info.get_language_code()}")
                 return None
-            return TranslationUnit(f"{page}/{identifier}", language_info.get_language_code(),
+            return TranslationUnit(f"{page}/{identifier}", language_info.get_language_code(), \
                 "", content) # Leaving definition parameter empty because we don't have it and don't need it
 
         elif identifier == self.TITLE:
             worksheet_info: Optional[WorksheetInfo] = language_info.get_worksheet(page)
             if worksheet_info is not None:
-                return TranslationUnit(f"{page}/{language_info.get_language_code()}",
-                    language_info.get_language_code(), page, worksheet_info.title)
+                return TranslationUnit(f"{page}/Page display title",
+                                       language_info.get_language_code(), page, worksheet_info.title)
             content = fortraininglib.get_translated_title(page, language_info.get_language_code())
             if content is None:
                 self.logger.info(f"Couldn't load {page}/{identifier}/{language_info.get_language_code()}")
                 return None
             return TranslationUnit(f"{page}/Page display title",
-                language_info.get_language_code(), page, content)
+                                   language_info.get_language_code(), page, content)
 
         else:
             raise LookupError(f"Invalid unit name {page}/{identifier}/{language_info.get_language_code()}")
 
-    def should_be_equal(self, base: Optional[TranslationUnit], other: Optional[TranslationUnit]):
+    def should_be_equal(self, base: Optional[TranslationUnit], other: Optional[TranslationUnit]) -> bool:
+        """returns True if checks pass: base and other are the same (or not existing)"""
         if base is None or other is None:
-            return
+            return True
         if other.get_translation() == base.get_translation():
             self.logger.info(f"Consistency check passed: {base.get_translation()} == {other.get_translation()}")
-        else:
-            self.logger.warning(f"Consistency check failed: {other.get_translation()} is not equal to "
-                                f"{base.get_translation()}. Check {base.get_name()} and {other.get_name()}")
+            return True
+        self.logger.warning(f"Consistency check failed: {other.get_translation()} is not equal to "
+                            f"{base.get_translation()}. Check {base.get_name()} and {other.get_name()}")
+        return False
 
-    def should_start_with(self, base: Optional[TranslationUnit], other: Optional[TranslationUnit]):
+    def should_start_with(self, base: Optional[TranslationUnit], other: Optional[TranslationUnit]) -> bool:
+        """returns True if checks pass: other starts with base (or not existing)"""
         if base is None or other is None:
-            return
+            return True
         if other.get_translation().startswith(base.get_translation()):
-            self.logger.info(f"Consistency check passed: {other.get_translation()} starts with {base.get_translation()}.")
-        else:
-            self.logger.warning(f"Consistency check failed: {other.get_translation()} does not start with "
-                                f"{base.get_translation()}. Check {base.get_name()} and {other.get_name()}")
+            self.logger.info(f"Consistency check passed: "
+                             f"{other.get_translation()} starts with {base.get_translation()}.")
+            return True
+        self.logger.warning(f"Consistency check failed: {other.get_translation()} does not start with "
+                            f"{base.get_translation()}. Check {base.get_name()} and {other.get_name()}")
+        return False
 
-    def check_bible_reading_hints_titles(self, language_info: LanguageInfo):
+    def check_bible_reading_hints_titles(self, language_info: LanguageInfo) -> bool:
         """Titles of the different Bible Reading Hints variants should start the same"""
-        self.should_start_with(
+        ret1 = self.should_start_with(
             self.load_translation_unit(language_info, "Bible_Reading_Hints", self.TITLE),
             self.load_translation_unit(language_info, "Bible_Reading_Hints_(Seven_Stories_full_of_Hope)", self.TITLE))
-        self.should_start_with(
+        ret2 = self.should_start_with(
             self.load_translation_unit(language_info, "Bible_Reading_Hints", self.TITLE),
             self.load_translation_unit(language_info, "Bible_Reading_Hints_(Starting_with_the_Creation)", self.TITLE))
+        return ret1 and ret2
 
-    def check_gods_story_titles(self, language_info: LanguageInfo):
+    def check_bible_reading_hints_links(self, language_info: LanguageInfo) -> bool:
+        """Check whether the link titles in https://www.4training.net/Bible_Reading_Hints
+        are identical with the titles of the destination pages"""
+        ret1 = True
+        ret2 = True
+        link_unit = self.load_translation_unit(language_info, "Bible_Reading_Hints", 2)
+        if link_unit is not None:
+            _, title = self.extract_link(link_unit.get_translation())
+            link_unit.set_translation(title)
+            ret1 = self.should_be_equal(link_unit, self.load_translation_unit(
+                language_info, "Bible_Reading_Hints_(Seven_Stories_full_of_Hope)", self.TITLE))
+        link_unit = self.load_translation_unit(language_info, "Bible_Reading_Hints", 3)
+        if link_unit is not None:
+            _, title = self.extract_link(link_unit.get_translation())
+            link_unit.set_translation(title)
+            ret2 = self.should_be_equal(link_unit, self.load_translation_unit(
+                language_info, "Bible_Reading_Hints_(Starting_with_the_Creation)", self.TITLE))
+        return ret1 and ret2
+
+
+    def check_gods_story_titles(self, language_info: LanguageInfo) -> bool:
         """Titles of the two different variants of God's Story should start the same"""
-        self.should_start_with(
+        ret1 = self.should_start_with(
             self.load_translation_unit(language_info, "God's_Story", self.TITLE),
             self.load_translation_unit(language_info, "God's_Story_(first_and_last_sacrifice)", self.TITLE))
-        self.should_start_with(
+        ret2 = self.should_start_with(
             self.load_translation_unit(language_info, "God's_Story", self.TITLE),
             self.load_translation_unit(language_info, "God's_Story_(five_fingers)", self.TITLE))
+        return ret1 and ret2
 
-    def check_who_do_i_need_to_forgive(self, language_info: LanguageInfo):
+    def check_who_do_i_need_to_forgive(self, language_info: LanguageInfo) -> bool:
         """Should both be 'God, who do I need to forgive?'"""
-        self.should_be_equal(self.load_translation_unit(language_info, "How_to_Continue_After_a_Prayer_Time", 11),
+        return self.should_be_equal(
+            self.load_translation_unit(language_info, "How_to_Continue_After_a_Prayer_Time", 11), \
             self.load_translation_unit(language_info, "Forgiving_Step_by_Step", 34))
+
+    def check_book_of_acts(self, language_info: LanguageInfo) -> bool:
+        """Name of the book of Acts should be the same in different Bible Reading Hints variants"""
+        t24 = self.load_translation_unit(language_info, "Template:BibleReadingHints", 24)
+        t26 = self.load_translation_unit(language_info, "Template:BibleReadingHints", 26)
+        if t24 is None or (len(t24.get_translation()) <= 3) or t26 is None or (len(t26.get_translation()) <= 3):
+            return
+        # Text is e.g. "2. Apostelgeschichte" / "3. Apostelgeschichte" -> remove first three characters
+        t24.set_translation(t24.get_translation()[3:])
+        t26.set_translation(t26.get_translation()[3:])
+        return self.should_be_equal(t24, t26)
 
     def run(self, language_info: LanguageInfo, change_log: ChangeLog):
         self.check_bible_reading_hints_titles(language_info)
         self.check_gods_story_titles(language_info)
         self.check_who_do_i_need_to_forgive(language_info)
+        self.check_bible_reading_hints_links(language_info)
+        self.check_book_of_acts(language_info)
 
 """
 TODO: implement the following checks:
-Should be the same:
-Translations:Bible Reading Hints (Seven Stories full of Hope)/Page display title/de
-The link title in Translations:Bible Reading Hints/2/ru
-
-Should be the same:
-Translations:Bible Reading Hints (Starting with the Creation)/Page display title/de
-The link title in Translations:Bible Reading Hints/2/ru
 
 Head-Heart-Hands questions should be the same on
-https://www.4training.net/Time_with_God/tr-tanri
+https://www.4training.net/Time_with_God
 https://www.4training.net/Template:BibleReadingHints
+TODO no: the first uses "me", the latter "we"
 
+TODO: this can't really be automated, needs to be checked manually
 Many title from the Three-Thirds-Process and from Template:BibleReadingHints should be the same
-
-Template:BibleReadingHints translation of "Acts" in should be the same in both variants:
-Translations:Template:BibleReadingHints/24/de
-Translations:Template:BibleReadingHints/26/de
 
 Each list item of the Seven Stories full of hope starts with a number (in the target language of course...)
 e.g. Translations:Bible Reading Hints (Seven Stories full of Hope)/7/id starts with "1."
+
+Check each link: Is the title the same as the title of the destination page?
 """
---
+++ pywikitools/test/test_consistency_checks.py
@@ -0,0 +1,29 @@
+import unittest
+from pywikitools.resourcesbot.changes import ChangeLog
+
+from pywikitools.resourcesbot.consistency_checks import ConsistencyCheck
+from pywikitools.resourcesbot.data_structures import LanguageInfo
+
+class TestConsistencyCheck(unittest.TestCase):
+    def test_extract_link(self):
+        cc = ConsistencyCheck()
+        dest, title = cc.extract_link("Here is a link: [[Destination|Title]].")
+        self.assertEqual(dest, "Destination")
+        self.assertEqual(title, "Title")
+        # Now check that empty strings are returned when there is no link in the translation unit
+        dest, title = cc.extract_link("Nothing. [[This link won't be considered.]]")
+        self.assertEqual(dest, "")
+        self.assertEqual(title, "")
+
+    def test_everything_in_english(self):
+        """All consistency checks should pass in English"""
+        cc = ConsistencyCheck()
+        language_info = LanguageInfo("en")
+        self.assertTrue(cc.check_bible_reading_hints_titles(language_info))
+        self.assertTrue(cc.check_gods_story_titles(language_info))
+        self.assertTrue(cc.check_who_do_i_need_to_forgive(language_info))
+        self.assertTrue(cc.check_bible_reading_hints_links(language_info))
+        self.assertTrue(cc.check_book_of_acts(language_info))
+
+if __name__ == '__main__':
+    unittest.main()
