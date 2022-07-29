0289d1b88aa39bae33e7231f7504ace555158f9a
holybiber <samuel@holydevelopers.net>
2022-07-30 00:09:22 +0200

    ResourcesBot: Change column order in reports
    In class WriteReport - for better readability

--- pywikitools/resourcesbot/write_report.py
+++ pywikitools/resourcesbot/write_report.py
@@ -130,10 +130,10 @@ class WriteReport(LanguagePostProcessor):
             string with mediawiki code for a whole paragraph with the complete table
         """
         content: str = "== Worksheets ==\n"
-        content += '{| class="wikitable" style="width:100%"\n'
-        content += "|-\n! Worksheet\n! Translation\n! Progress\n! colspan=\"2\" | PDF\n! ODT\n! Version\n"
+        content += '{| class="wikitable" style="width:100%"\n|-\n'
         content += f"! [[{language_info.english_name}#Available_training_resources_in_{language_info.english_name}|"
-        content += "Listed?]]\n! CorrectBot\n"
+        content += "Listed?]]\n! Worksheet\n! Translation\n! Progress\n! colspan=\"2\" | PDF\n! ODT\n! Version\n"
+        content += "! CorrectBot\n"
         for page, en_worksheet in english_info.worksheets.items():
             lang_worksheet = language_info.worksheets[page] if page in language_info.worksheets else None
             content += self.create_worksheet_line(language_info.language_code, en_worksheet, lang_worksheet)
@@ -152,16 +152,22 @@ class WriteReport(LanguagePostProcessor):
         Returns:
             string with mediawiki code for one line of our table
         """
-        # column 1: Link to English worksheet
-        content: str = f"| [[{en_worksheet.title}]]\n"
+        # column 1: Is this worksheet listed in the language overview page?
+        if lang_worksheet is not None and lang_worksheet.show_in_list(en_worksheet):
+            content = "| style=\"text-align:center\" | ✓\n"
+        else:
+            content = "| style=\"text-align:center\" | -\n"
 
-        # column 2: Link to translated worksheet (if existing)
+        # column 2: Link to English worksheet
+        content += f"| [[{en_worksheet.title}]]\n"
+
+        # column 3: Link to translated worksheet (if existing)
         if lang_worksheet is not None:
             content += f"| [[{en_worksheet.title}/{language_code}|{lang_worksheet.title}]]\n"
         else:
             content += "| -\n"
 
-        # column 7: Version information (we need to process this here because version_color is needed for other columns)
+        # column 8: Version information (we need to process this here because version_color is needed for other columns)
         version_color = Color.RED
         if lang_worksheet is None:
             version_content = f'| style="background-color:{Color.RED}" | -\n'
@@ -172,7 +178,7 @@ class WriteReport(LanguagePostProcessor):
             version_content = f'| style="background-color:{Color.RED}" '
             version_content += f"| {lang_worksheet.version} (Original: {en_worksheet.version})\n"
 
-        # column 3: Translation progress
+        # column 4: Translation progress
         translated_unit_count: int = lang_worksheet.progress.translated if lang_worksheet is not None else 0
         progress: int = round(translated_unit_count / en_worksheet.progress.total * 100)
         if lang_worksheet is None:
@@ -193,7 +199,7 @@ class WriteReport(LanguagePostProcessor):
         content += f"&action=page&filter={'' if progress == 100 else '!'}translated"
         content += f"&language={language_code} {progress}%]\n"
 
-        # column 4: Link to translated PDF file (if existing)
+        # column 5: Link to translated PDF file (if existing)
         if lang_worksheet is not None and (file_info := lang_worksheet.get_file_type_info("pdf")) is not None:
             pdf_color = Color.GREEN if version_color == Color.GREEN else Color.ORANGE
             if file_info.metadata is not None and not file_info.metadata.correct:
@@ -206,7 +212,7 @@ class WriteReport(LanguagePostProcessor):
             content += f'| style="background-color:{pdf_color}" '
             content += f"| [[File:{lang_worksheet.get_file_type_name('pdf')}]]\n"
 
-            # column 5: PDF metadata details
+            # column 6: PDF metadata details
             if file_info.metadata is not None:
                 content += f'| style="background-color:{pdf_color}" | {file_info.metadata.to_html()}\n'
             else:
@@ -215,7 +221,7 @@ class WriteReport(LanguagePostProcessor):
             pdf_color = Color.RED
             content += f'| colspan="2" style="background-color:{Color.RED}; text-align:center" | -\n'
 
-        # column 6: Link to translated ODT file (if existing)
+        # column 7: Link to translated ODT file (if existing)
         if lang_worksheet is not None and lang_worksheet.has_file_type("odt"):
             odt_color = Color.GREEN if version_color == Color.GREEN else Color.ORANGE
             content += f'| style="background-color:{odt_color}" '
@@ -226,12 +232,6 @@ class WriteReport(LanguagePostProcessor):
 
         # Now we append content for column 7: version information
         content += version_content
-
-        # column 8: Is this worksheet listed in the language overview page?
-        if lang_worksheet is not None and lang_worksheet.show_in_list(en_worksheet):
-            content += "| style=\"text-align:center\" | ✓\n"
-        else:
-            content += "| style=\"text-align:center\" | -\n"
 
         # column 9: CorrectBot status (do we have an up-to-date report?)
         if lang_worksheet is not None:
