2e34ef614edc95fbf95246e0534abc8145803c91
holybiber <samuel@holydevelopers.net>
2022-10-14 15:17:47 +0200

    CorrectBot: Change quotation style in Arabic

--- pywikitools/correctbot/correctors/ar.py
+++ pywikitools/correctbot/correctors/ar.py
@@ -6,12 +6,13 @@ from .universal import QuotationMarkCorrector, UniversalCorrector, RTLCorrector
 class ArabicCorrector(CorrectorBase, UniversalCorrector, RTLCorrector, QuotationMarkCorrector):
     """
     Correct Arabic typos:
-    * Arabic quotations start with start with « and end with »
+    * Arabic quotations start with start with “ and end with ”
+      (at least that's what we're doing for now, in some contexts also «quote» is used)
     * Common corrections from UniversalCorrector and RTLCorrector
     """
     def correct_quotes(self, text: str) -> str:
-        """Ensure correct Arabic quotes (example: «اقتباس»)"""
-        return self._correct_quotes('«', '»', text)
+        """Ensure correct Arabic quotes (example: “اقتباس”)"""
+        return self._correct_quotes('“', '”', text)
 
     def _capitalization_exceptions(self) -> List[str]:
         return []
