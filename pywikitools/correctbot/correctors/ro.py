e0f952f6eee8c909cae8f908d1814abbd8bdacbb
holybiber <samuel@holydevelopers.net>
2022-10-15 17:27:34 +0200

    CorrectBot: Romanian rule to replace Ş/ş with Ș/ș

--- pywikitools/correctbot/correctors/ro.py
+++ pywikitools/correctbot/correctors/ro.py
@@ -14,6 +14,12 @@ class RomanianCorrector(CorrectorBase, UniversalCorrector, NoSpaceBeforePunctuat
         """Ensure correct Romanian quotes (example: „quote”)"""
         return self._correct_quotes('„', '”', text)
 
+    def correct_s_comma(self, text: str) -> str:
+        """Replace Ş/ş with the correct Ș/ș in Romanian"""
+        # See https://en.wikipedia.org/wiki/S-comma
+        text = text.replace("Ş", "Ș")
+        return text.replace("ş", "ș")
+
     def _capitalization_exceptions(self) -> List[str]:
         return ["ex.", "Ex.", "ș.a.m.d.", "ș.a.m.", "ș.a.", "Ș.a.m.d.", "Ș.a.m.", "Ș.a."]
         # TODO only adding "ș.a.m.d." is not enough, that's currently a workaround, find a better solution
