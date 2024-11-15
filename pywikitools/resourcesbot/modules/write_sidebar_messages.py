f97fb560c11fa793e7483a95c04763d092205272
Gabriel Sousa <gabriel@gsousa.com.br>
2024-11-15 12:50:24 -0300

    chore[resourcesbot]: reformat `write_sidebar_messages` with `black`

--- pywikitools/resourcesbot/modules/write_sidebar_messages.py
+++ pywikitools/resourcesbot/modules/write_sidebar_messages.py
@@ -3,31 +3,36 @@ from configparser import ConfigParser
 from typing import Final
 
 import pywikibot
-from pywikitools.resourcesbot.changes import ChangeLog, ChangeType
-from pywikitools.resourcesbot.modules.post_processing import LanguagePostProcessor
-from pywikitools.resourcesbot.data_structures import LanguageInfo, WorksheetInfo
+
 from pywikitools.fortraininglib import ForTrainingLib
+from pywikitools.resourcesbot.changes import ChangeLog, ChangeType
+from pywikitools.resourcesbot.data_structures import LanguageInfo, WorksheetInfo
+from pywikitools.resourcesbot.modules.post_processing import LanguagePostProcessor
 
 
 class WriteSidebarMessages(LanguagePostProcessor):
     """
-    Write/update the system messages for the sidebar with the translated titles of worksheets.
-    These are used when displaying the whole website in another language (changing the "user interface language")
+    Write/update the system messages for the sidebar with the translated
+    titles of worksheets.
+    These are used when displaying the whole website in another language
+    (changing the "user interface language")
 
-    E.g. write German headline of "Hearing from God"
+    E.g., write German headline of "Hearing from God"
     to https://www.4training.net/MediaWiki:Sidebar-hearingfromgod/de
 
-    More information on system messages: https://www.mediawiki.org/wiki/Help:System_message
+    More information on system messages:
+    https://www.mediawiki.org/wiki/Help:System_message
 
     This class can be re-used to call run() several times
     """
+
     def __init__(
-            self,
-            fortraininglib: ForTrainingLib,
-            config: ConfigParser,
-            site: pywikibot.site.APISite,
-            *,
-            force_rewrite: bool = False,
+        self,
+        fortraininglib: ForTrainingLib,
+        config: ConfigParser,
+        site: pywikibot.site.APISite,
+        *,
+        force_rewrite: bool = False,
     ):
         """
         Args:
@@ -35,12 +40,15 @@ class WriteSidebarMessages(LanguagePostProcessor):
         """
         super().__init__(fortraininglib, config, site, force_rewrite=force_rewrite)
         self.logger: Final[logging.Logger] = logging.getLogger(
-            'pywikitools.resourcesbot.modules.write_sidebar_messages'
+            "pywikitools.resourcesbot.modules.write_sidebar_messages"
         )
 
     def save_worksheet_title(self, worksheet: WorksheetInfo):
         """Save system message with the title of the given worksheet."""
-        title = f"MediaWiki:{self.fortraininglib.title_to_message(worksheet.page).capitalize()}"
+        title = (
+            f"MediaWiki:"
+            f"{self.fortraininglib.title_to_message(worksheet.page).capitalize()}"
+        )
         if worksheet.language_code != "en":
             title += f"/{worksheet.language_code}"
         self.logger.debug(f"save_worksheet_title(): title = {title}")
@@ -58,16 +66,25 @@ class WriteSidebarMessages(LanguagePostProcessor):
     def has_relevant_change(worksheet: str, changes: ChangeLog) -> bool:
         """
         Is there a relevant change for our worksheet?
-        Relevant is a change indicating that the translated title might have changed (new / updated worksheet)
+        Relevant is a change indicating that the translated title might have changed
+        (new / updated worksheet)
         """
         for change_item in changes:
             if change_item.worksheet == worksheet:
-                if change_item.change_type == ChangeType.NEW_WORKSHEET or \
-                   change_item.change_type == ChangeType.UPDATED_WORKSHEET:
+                if (
+                    change_item.change_type == ChangeType.NEW_WORKSHEET
+                    or change_item.change_type == ChangeType.UPDATED_WORKSHEET
+                ):
                     return True
         return False
 
-    def run(self, language_info: LanguageInfo, _english_info, changes: ChangeLog, _english_changes) -> None:
+    def run(
+        self,
+        language_info: LanguageInfo,
+        _english_info,
+        changes: ChangeLog,
+        _english_changes,
+    ) -> None:
         """Our entry function"""
         for worksheet in language_info.worksheets.values():
             if worksheet.title == "":
