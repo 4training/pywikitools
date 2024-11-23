cbc2fb6b6170b9d0577e3da523ad489e02b6d936
holybiber <samuel@holydevelopers.net>
2024-11-23 11:35:01 +0100

    Add corrector for Uzbek (Cyrillic script)

---
+++ pywikitools/correctbot/correctors/uz-cyrl.py
@@ -0,0 +1,26 @@
+
+from typing import List
+from .base import CorrectorBase
+from .universal import NoSpaceBeforePunctuationCorrector, QuotationMarkCorrector, UniversalCorrector
+
+
+class UzbekCyrillicCorrector(
+    CorrectorBase, UniversalCorrector, NoSpaceBeforePunctuationCorrector, QuotationMarkCorrector
+):
+    """
+    Correct Uzbek (Cyrillic script) typos:
+    * Uzbek quotations start with “ and end with ”
+    * Common corrections from UniversalCorrector and NoSpaceBeforePunctuationCorrector
+    """
+    def correct_quotes(self, text: str) -> str:
+        """Ensure correct Uzbek quotes (example: “кечириш”)"""
+        return self._correct_quotes('“', '”', text)
+
+    def _suffix_for_print_version(self) -> str:
+        return "_print"
+
+    def _capitalization_exceptions(self) -> List[str]:
+        return []
+
+    def _missing_spaces_exceptions(self) -> List[str]:
+        return []
