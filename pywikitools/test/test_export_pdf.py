787162631b1ebfa775de9ce95b3e91f252891039
holybiber <samuel@holydevelopers.net>
2024-12-02 12:04:27 +0100

    ResourcesBot: Improve testing of ExportPDF

--- pywikitools/test/test_export_pdf.py
+++ pywikitools/test/test_export_pdf.py
@@ -15,7 +15,6 @@ from os.path import abspath, dirname, join, exists
 import json
 import requests
 import tempfile
-from pywikitools.fortraininglib import ForTrainingLib
 from pywikitools.resourcesbot.changes import ChangeLog, ChangeType
 from pywikitools.resourcesbot.data_structures import LanguageInfo, json_decode
 from pywikitools.resourcesbot.modules.export_pdf import ExportPDF
@@ -30,25 +29,24 @@ class TestExportPDF(unittest.TestCase):
         self.english_info = LanguageInfo("en", "English")
         for worksheet, info in self.language_info.worksheets.items():
             self.english_info.add_worksheet_info(worksheet, info)
-        self.fortraininglib = ForTrainingLib("https://test.4training.net")
 
     @patch("os.makedirs")
     def test_run_with_empty_base_folder(self, mock_makedirs):
         empty_config = ConfigParser()
         with self.assertLogs('pywikitools.resourcesbot.modules.export_pdf', level='WARNING'):
-            export_pdf = ExportPDF(self.fortraininglib, empty_config, None)
+            export_pdf = ExportPDF(Mock(), empty_config, None)
 
         # run() should return without doing anything because of empty base folder
         export_pdf.run(self.language_info, self.english_info, ChangeLog(), ChangeLog(), force_rewrite=False)
         mock_makedirs.assert_not_called()
 
-    def test_run_filters_unfinished_worksheets(self):
-        fortraininglib_mock = Mock()
+    @patch('requests.get')
+    def test_run_filters_unfinished_worksheets(self, mock_requests_get):
         temp_config = ConfigParser()
         temp_config.add_section("Paths")
         with tempfile.TemporaryDirectory() as temp_dir:
             temp_config.set("Paths", "pdfexport", temp_dir)
-            export_pdf = ExportPDF(fortraininglib_mock, temp_config, None)
+            export_pdf = ExportPDF(Mock(), temp_config, None)
             with patch.object(export_pdf, 'has_relevant_change', return_value=False) as mock_has_relevant_change:
                 export_pdf.run(self.language_info, self.english_info, ChangeLog(), ChangeLog(), force_rewrite=False)
                 calls = [call[0][0] for call in mock_has_relevant_change.call_args_list]
@@ -56,7 +54,7 @@ class TestExportPDF(unittest.TestCase):
                 self.assertIn('Healing', calls)
                 self.assertNotIn('Church', calls)
 
-            fortraininglib_mock.get_page_html.assert_not_called()
+            mock_requests_get.assert_not_called()
             # Verify that `language_info` remains unchanged
             self.assertIsNotNone(self.language_info.get_worksheet('Church'))
 
@@ -69,7 +67,7 @@ class TestExportPDF(unittest.TestCase):
             config.set("Paths", "pdfexport", base_folder)
 
             # Base folder should be created directly when initializing the class
-            export_pdf = ExportPDF(self.fortraininglib, config, None)
+            export_pdf = ExportPDF(Mock(), config, None)
             self.assertTrue(exists(base_folder))
             export_pdf.run(self.language_info, self.english_info, ChangeLog(), ChangeLog(), force_rewrite=False)
 
@@ -80,7 +78,7 @@ class TestExportPDF(unittest.TestCase):
             with self.assertNoLogs(level='WARNING'):
                 export_pdf.run(self.language_info, self.english_info, ChangeLog(), ChangeLog(), force_rewrite=False)
 
-    def test_download_and_save_transformed_html_and_images(self):
+    def test_download_and_save_pdf(self):
         changelog = ChangeLog()
         changelog.add_change('Healing', ChangeType.UPDATED_WORKSHEET)
         changelog.add_change("Church", ChangeType.NEW_WORKSHEET)
@@ -88,29 +86,24 @@ class TestExportPDF(unittest.TestCase):
         config = ConfigParser()
         config.add_section("Paths")
 
-        # Mock the response for the image download
+        # Mock the response for the pdf download
         response = requests.Response()
         response.status_code = 200
-        with open(join(dirname(abspath(__file__)), "data", "Исцеление.pdf"), 'rb') as f:
-            response._content = f.read()
+        response._content = b'Dummy PDF content'
 
-        # Initialize the ExportHTML class with a valid base folder
+        # Initialize the ExportPDF class with a valid base folder
         with tempfile.TemporaryDirectory() as temp_dir:
             config.set("Paths", "pdfexport", temp_dir)
-            export_pdf = ExportPDF(self.fortraininglib, config, None)
+            export_pdf = ExportPDF(Mock(), config, None)
 
             with patch('requests.get', return_value=response):
                 export_pdf.run(self.language_info, self.english_info, changelog, ChangeLog(), force_rewrite=False)
 
-            # Assert the file was created correctly
+            # Assert the file was created correctly and the content is correct
             expected_path_to_pdf = join(temp_dir, 'ru', 'Исцеление.pdf')
             self.assertTrue(exists(expected_path_to_pdf))
-
-            # Assert the content is correct
-            expected_pdf = join(dirname(abspath(__file__)), "data", "Исцеление.pdf")
             with open(expected_path_to_pdf, 'rb') as test_file:
-                with open(expected_pdf, 'rb') as expected_file:
-                    self.assertEqual(test_file.read(), expected_file.read())
+                self.assertEqual(test_file.read(), b'Dummy PDF content')
 
     def test_complex_export_pdf(self):
         config = ConfigParser()
@@ -122,9 +115,6 @@ class TestExportPDF(unittest.TestCase):
             # normal worksheet
             ar_changelog.add_change('Hearing_from_God', ChangeType.UPDATED_WORKSHEET)
             expected_path_hearing = join(temp_dir, 'ar', 'الاستماع_من_الله.pdf')
-            # worksheet with images
-            ar_changelog.add_change('Time_with_God', ChangeType.UPDATED_WORKSHEET)
-            expected_path_time = join(temp_dir, 'ar', 'قضاء_وقت_مع_الله.pdf')
             # unfinished worksheet -> shouldn't be exported
             ar_changelog.add_change("Church", ChangeType.NEW_WORKSHEET)
             expected_path_church = join(temp_dir, 'ar', 'كنيسة.pdf')
@@ -136,17 +126,15 @@ class TestExportPDF(unittest.TestCase):
             with open(join(dirname(abspath(__file__)), "data", "en.json"), 'r') as f:
                 en_language_info: LanguageInfo = json.load(f, object_hook=json_decode)
 
-            export_pdf = ExportPDF(self.fortraininglib, config, None)
+            export_pdf = ExportPDF(Mock(), config, None)
             export_pdf.run(ar_language_info, en_language_info, ar_changelog, ChangeLog(), force_rewrite=False)
 
             self.assertTrue(exists(expected_path_hearing))
-            self.assertTrue(exists(expected_path_time))
             self.assertFalse(exists(expected_path_church))
 
             # run with force rewrite
             self.assertFalse(exists(expected_path_prayer))
-            with self.assertLogs():
-                export_pdf.run(ar_language_info, en_language_info, ar_changelog, ChangeLog(), force_rewrite=True)
+            export_pdf.run(ar_language_info, en_language_info, ar_changelog, ChangeLog(), force_rewrite=True)
             self.assertTrue(exists(expected_path_prayer))
 
 
