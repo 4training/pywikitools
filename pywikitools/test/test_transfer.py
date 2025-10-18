ace241f46fa9d38818ccf57e9c266bfaf5d54b96
TobiasBeck777 <tobias.beck.89@gmail.com>
2025-10-18 13:59:02 +0200

    test of transfer

--- pywikitools/test/test_transfer.py
+++ pywikitools/test/test_transfer.py
@@ -12,21 +12,21 @@ class TestTransferTool(unittest.TestCase):
         mock_pywikibot_site.return_value.logged_in.return_value = True
         config = ConfigParser()
         config.read_dict({"transfer": {"source_username": "User1", 
-                                       "source_site": "local",
+                                       "source_site": "test",
                                        "destination_username": "User2", 
-                                       "destination_site": "test"
+                                       "destination_site": "local"
                                        }})
         self.transfer_tool = TransferTool(config)
 
     @patch('pywikibot.Page')
     def test_upload(self, mock_page):
         self.transfer_tool.upload("Test_Page/1/fr", "Test transfer")
-        mock_page.return_value.save.assert_called_once_with(summary="Transfer of 'Test_Page/1/fr' from 'local' to 'test' completed")
+        mock_page.return_value.save.assert_called_once_with(summary="Transfer of 'Test_Page/1/fr' from 'test' to 'local'")
 
     @patch('pywikibot.Page')
     def test_transfer(self, mock_page):
-        self.transfer_tool.transfer("", "fr", )
-        mock_page.return_value.save.assert_called_once_with(summary="Transfer of 'Test_Page/1/fr' from 'local' to 'test' completed")
+        self.transfer_tool.transfer("A_Daily_Prayer", "fr")
+        mock_page.return_value.save.assert_called_with(summary="Transfer of 'A_Daily_Prayer/21/fr' from 'test' to 'local'")
 
 
 if __name__ == "__main__":
