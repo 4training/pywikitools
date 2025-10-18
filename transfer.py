7d1407b772ba770103b28b614631d59366d42d58
TobiasBeck777 <tobias.beck.89@gmail.com>
2025-10-18 14:36:51 +0200

    formatting

--- transfer.py
+++ transfer.py
@@ -1,6 +1,5 @@
 import logging
 from os.path import abspath, dirname, join
-import requests
 import pywikibot
 import argparse
 from pywikitools.family import Family
@@ -8,6 +7,7 @@ from pywikitools.fortraininglib import ForTrainingLib
 from configparser import ConfigParser
 
 TIMEOUT: int = 30           # Timeout after 30s (prevent indefinite hanging when there is network issues)
+
 
 class TransferTool:
     def __init__(self, config: ConfigParser):
@@ -24,8 +24,8 @@ class TransferTool:
 
         family = Family()
 
-        self.source_wiki_site = pywikibot.Site(code=self.source_site, fam=family, 
-                                          user=config.get('transfer', 'source_username'))
+        self.source_wiki_site = pywikibot.Site(code=self.source_site, fam=family,
+                                               user=config.get('transfer', 'source_username'))
         if not self.source_wiki_site.logged_in():
             self.source_wiki_site.login()
             if not self.source_wiki_site.logged_in():
@@ -33,8 +33,8 @@ class TransferTool:
         # Set throttle to 0 to speed up write operations (otherwise pywikibot would wait up to 10s after each write)
         self.source_wiki_site.throttle.setDelays(delay=0, writedelay=0, absolute=True)
 
-        self.destination_wiki_site = pywikibot.Site(code=self.destination_site, fam=family, 
-                                               user=config.get('transfer', 'destination_username'))
+        self.destination_wiki_site = pywikibot.Site(code=self.destination_site, fam=family,
+                                                    user=config.get('transfer', 'destination_username'))
         if not self.destination_wiki_site.logged_in():
             self.destination_wiki_site.login()
             if not self.destination_wiki_site.logged_in():
@@ -43,28 +43,27 @@ class TransferTool:
         self.destination_wiki_site.throttle.setDelays(delay=0, writedelay=0, absolute=True)
 
         self.source_fortraininglib: ForTrainingLib = ForTrainingLib(family.base_url(self.source_site, ''),
-                                                             family.scriptpath(self.source_site))
+                                                                    family.scriptpath(self.source_site))
         self.destination_fortraininglib: ForTrainingLib = ForTrainingLib(family.base_url(self.destination_site, ''),
-                                                             family.scriptpath(self.destination_site))
-
+                                                                         family.scriptpath(self.destination_site))
 
     def transfer(self, page_name, language_code):
         source_translation_page = self.source_fortraininglib.get_translation_units(page_name, "en")
 
         for translation_unit in source_translation_page:
-            self.upload(f"{translation_unit.identifier}/{language_code}",
-                                    translation_unit.get_translation())
-
+            self.upload(f"{translation_unit.identifier}/{language_code}", translation_unit.get_translation())
 
     def upload(self, identifier: str, translated_text: str):
         """Transfer a workshoot from one mediawiki system to another one"""
         destination_mediawiki_page = pywikibot.Page(self.destination_wiki_site, f"Translations:{identifier}")
         destination_mediawiki_page.text = translated_text
-        destination_mediawiki_page.save(summary=f"Transfer of '{identifier}' from '{self.source_site}' to '{self.destination_site}'")
+        destination_mediawiki_page.save(
+            summary=f"Transfer of '{identifier}' from '{self.source_site}' to '{self.destination_site}'")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Transfer a worksheet for a certain language from one site to another site.")
+    parser = argparse.ArgumentParser(
+        description="Transfer a worksheet for a certain language from one site to another site.")
     parser.add_argument("worksheet_name", help="Name of the worksheet to transfer.")
     parser.add_argument("language_code", help="Target language code for transfer.")
     args = parser.parse_args()
@@ -74,4 +73,3 @@ if __name__ == "__main__":
 
     transfer_tool = TransferTool(config)
     transfer_tool.transfer(args.worksheet_name, args.language_code)
-
